8e2ab13fa53ed11c0983d00c3d3a87cab21abe54
harshit-tiwari-codebase <harshit20112005@gmail.com>
2026-06-05 19:51:02 +0530

    Configure CSP, CORS, and serve updated frontend build

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 app.use(express.json());
 const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://pixlify-mern-1.onrender.com"],
     credentials: true,
   })
 );
@@ -13,6 +14,21 @@ app.use(
 // Register cookie-parser middleware
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    [
+      "default-src 'self'",
+      "script-src 'self'",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+      "font-src 'self' data: https://fonts.gstatic.com",
+      "img-src 'self' data: blob:",
+      "connect-src 'self' http://localhost:3000 http://localhost:5173 https://pixlify-mern-1.onrender.com",
+    ].join("; ")
+  );
+  next();
+});
 
 
 
@@ -33,6 +49,12 @@ const postRouter = require("./routes/post.routes");
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/user", followRouter);
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 
 module.exports = app;
