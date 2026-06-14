1a673a36b10a73fbb6e1f167703dc087cc4de2c6
harshit-tiwari-codebase <harshit20112005@gmail.com>
2026-06-14 17:47:39 +0530

    feat : create comment and getcomment api

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -1,17 +1,74 @@
 const commentModel = require("../models/comments.models");
+const postModel = require("../models/post.models");
 
-async function createComment(req , res) {
-    const {postId} = req.params;
+async function createComment(req, res) {
+  try {
+    const { postId } = req.params;
     const userId = req.user.id;
-    const comment = req.body.comment;
+    const { comment } = req.body;
+
+    if (!comment?.trim()) {
+      return res.status(400).json({
+        message: "Comment is required",
+      });
+    }
+
+    const post = await postModel.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    const newComment = await commentModel.create({
+      postId,
+      comment,
+      userId,
+    });
+
+    return res.status(201).json({
+      message: "Comment created successfully",
+      comment: newComment,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
+async function getComments (req , res){
+     try {
+    const { postId } = req.params;
+    
+
+   
+
+  
+ const response = await commentModel
+  .find({ postId })
+  .populate("userId", "username profile_img")
+  .sort({ createdAt: -1 });
+    
 
     
 
-    res.status(200).json({
-        message : `comment created successfully by ${userId}`,
-        comment,
-        postId
-    })
+    return res.status(200).json({
+      message: "Comments fetched successfully",
+       response
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 }
 
-module.exports = {createComment}
+module.exports = { createComment , getComments };
--- Backend/src/routes/comment.routes.js
+++ Backend/src/routes/comment.routes.js
@@ -7,7 +7,8 @@ const identifyUser = require("../middlewares/auth.middleware");
 const commentController = require("../controllers/comment.controller");
 
 
-commentRouter.post("/create/:postId" , identifyUser , commentController.createComment)
+commentRouter.post("/create/:postId" , identifyUser , commentController.createComment);
+commentRouter.get("/get-comments/:postId" , identifyUser , commentController.getComments);
 
 module.exports = commentRouter;
 
