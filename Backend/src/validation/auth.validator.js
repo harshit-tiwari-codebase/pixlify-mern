3062918925d9b51411bc6aeb6e7fdeb95c01270a
harshit-tiwari-codebase <harshit20112005@gmail.com>
2026-06-16 21:32:18 +0530

    feat : implemented express validator in backend

---
+++ Backend/src/validation/auth.validator.js
@@ -0,0 +1,46 @@
+import {body, validationResult } from "express-validator";
+
+function validate ( req, res , next){
+    const errors = validationResult(req);
+
+    if(errors.isEmpty()){
+        return next();
+    }
+
+    res.status(400).json({
+        errors : errors.array()
+    })
+
+}
+
+
+
+export const registerValidation = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Username must be between 3 and 20 characters"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number"),
+
+  validate, // ← important
+];
