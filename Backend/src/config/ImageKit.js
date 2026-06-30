cc36c0bb734672958b1cf5741a69f5fa8cd63688
harshit-tiwari-codebase <harshit20112005@gmail.com>
2026-07-01 00:33:04 +0530

    updating logs for deployement

--- Backend/src/config/ImageKit.js
+++ Backend/src/config/ImageKit.js
@@ -1,9 +1,17 @@
 const ImageKit = require("imagekit");
 
+if (
+  !process.env.IMAGEKIT_PUBLIC_KEY ||
+  !process.env.IMAGEKIT_PRIVATE_KEY ||
+  !process.env.IMAGEKIT_URL_ENDPOINT
+) {
+  throw new Error("ImageKit environment variables are missing.");
+}
+
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
-module.exports = imagekit; // ✅ Correct
+module.exports = imagekit;
